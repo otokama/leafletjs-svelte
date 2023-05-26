80867cfa6e1a93554c31450322fc3d127c8a98e5
kai <flynn98721@gmail.com>
2023-05-26 13:03:07 -0400

    marker default url

--- src/lib/components/ui-layers/Marker.svelte
+++ src/lib/components/ui-layers/Marker.svelte
@@ -9,8 +9,8 @@
 	export let marker: Marker | undefined = undefined;
 
   setContext(L.Marker, () => marker);
-  L.Icon.Default.prototype.options.iconUrl = '/marker-icon.png';
-  L.Icon.Default.prototype.options.shadowUrl = '/marker-shadow.png';
+  // L.Icon.Default.prototype.options.iconUrl = '/marker-icon.png';
+  // L.Icon.Default.prototype.options.shadowUrl = '/marker-shadow.png';
 
 	$: if (!marker) {
     // update the default marker icon and shadow url
