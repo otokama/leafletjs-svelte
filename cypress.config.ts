50d24f357b6617ff2cf1b53684ec740f6848b700
kai <flynn98721@gmail.com>
2023-06-03 21:24:56 -0400

    updated cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+    "baseUrl": "http://localhost:8080",
+    "fixturesFolder": false,
+    "supportFile": false,
+    "screenshotOnRunFailure": false,
+    "video": false
+  }
 });
