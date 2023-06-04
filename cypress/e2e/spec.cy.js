e0badb5ecb5f7b07446fc13e01a4130fb49bdf23
kai <flynn98721@gmail.com>
2023-06-03 20:21:22 -0400

    adding cypress e2e testing

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,11 @@
+describe('spec.cy.js', () => {
+  it('should visit'), () => {
+    cy.visit('/');
+  }
+});
+
+describe('My First Test', () => {
+  it('Passed my first Cypress test!', () => {
+    expect(true).to.equal(true)
+  })
+});
