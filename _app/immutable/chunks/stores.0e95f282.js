b96c41efc89b8240304f09db2d50655b62e59472
kai <flynn98721@gmail.com>
2023-06-04 23:29:36 -0400

    Updates

---
+++ _app/immutable/chunks/stores.0e95f282.js
@@ -0,0 +1,1 @@
+import"./index.40f7ab64.js";import{s as e}from"./singletons.4258e10f.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
