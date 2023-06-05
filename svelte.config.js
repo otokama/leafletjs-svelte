6fdfe94738dff42d3a156dded610f19059fc8213
kai <flynn98721@gmail.com>
2023-06-04 22:05:39 -0400

    upload workflow

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,32 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { mdsvex } from 'mdsvex';
 
 const mdsvexOptions = {
-	extensions: [".md", ".svx"]
+  extensions: [".md", ".svx"]
 }
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
   extensions: [".svelte", ".md", ".svx"],
-	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
+  preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: undefined,
+      precompress: false,
+      strict: true
+    }),
+    paths: {
+      base: dev ? '' : process.env.BASE_PATH,
+    }
+  }
 };
 
 export default config;
