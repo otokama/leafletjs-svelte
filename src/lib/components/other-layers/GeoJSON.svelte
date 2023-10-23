83fa0f5806c55e63d5a92d85ed4c300e90a44dcf
kai <flynn98721@gmail.com>
2023-10-23 10:09:05 -0400

    formatting...

--- src/lib/components/other-layers/GeoJSON.svelte
+++ src/lib/components/other-layers/GeoJSON.svelte
@@ -1,45 +1,45 @@
 <script lang="ts">
-	import { leaflet as L } from '$lib/stores/leaflet.js';
-	import type { FeatureGroup, GeoJSON, GeoJSONOptions, LayerGroup, Map } from 'leaflet';
-	import { getContext, onDestroy, setContext } from 'svelte';
+  import { leaflet as L } from '$lib/stores/leaflet.js';
+  import type { FeatureGroup, GeoJSON, GeoJSONOptions, LayerGroup, Map } from 'leaflet';
+  import { getContext, onDestroy, setContext } from 'svelte';
 
-	export let data: any = undefined;
-	export let geojson: GeoJSON | undefined = undefined;
-	export let options: GeoJSONOptions | undefined = undefined;
+  export let data: any = undefined;
+  export let geojson: GeoJSON | undefined = undefined;
+  export let options: GeoJSONOptions | undefined = undefined;
 
-	const getMap: () => Map = getContext($L);
-	const getLayerGroup: () => LayerGroup = getContext($L.LayerGroup);
-	const getFeatureGroup: () => FeatureGroup = getContext($L.FeatureGroup);
-	setContext($L.Layer, () => geojson);
+  const getMap: () => Map = getContext($L);
+  const getLayerGroup: () => LayerGroup = getContext($L.LayerGroup);
+  const getFeatureGroup: () => FeatureGroup = getContext($L.FeatureGroup);
+  setContext($L.Layer, () => geojson);
 
-	$: if (!geojson) {
-		geojson = $L.geoJSON(data, options);
-		if (getLayerGroup) {
-			geojson.addTo(getLayerGroup());
-		}
-		if (getFeatureGroup) {
-			geojson.addTo(getFeatureGroup());
-		}
-		if (!getLayerGroup && !getFeatureGroup) {
-			geojson.addTo(getMap());
-		}
-	}
+  $: if (!geojson) {
+    geojson = $L.geoJSON(data, options);
+    if (getLayerGroup) {
+      geojson.addTo(getLayerGroup());
+    }
+    if (getFeatureGroup) {
+      geojson.addTo(getFeatureGroup());
+    }
+    if (!getLayerGroup && !getFeatureGroup) {
+      geojson.addTo(getMap());
+    }
+  }
 
-	onDestroy(() => {
-		if (geojson) {
-			if (getLayerGroup && getLayerGroup().hasLayer(geojson)) {
-				getLayerGroup().removeLayer(geojson);
-			}
-			if (getFeatureGroup && getFeatureGroup().hasLayer(geojson)) {
-				getFeatureGroup().removeLayer(geojson);
-			}
-			if (!getLayerGroup && !getFeatureGroup) {
-				geojson.removeFrom(getMap());
-			}
-		}
-	});
+  onDestroy(() => {
+    if (geojson) {
+      if (getLayerGroup && getLayerGroup().hasLayer(geojson)) {
+        getLayerGroup().removeLayer(geojson);
+      }
+      if (getFeatureGroup && getFeatureGroup().hasLayer(geojson)) {
+        getFeatureGroup().removeLayer(geojson);
+      }
+      if (!getLayerGroup && !getFeatureGroup) {
+        geojson.removeFrom(getMap());
+      }
+    }
+  });
 </script>
 
 {#if geojson}
-	<slot />
+  <slot />
 {/if}
